71befaf6f391c3f0663f367cb4ae583cdc9d610a
syh-alibaba <sunyuanhui.syh@alibaba-inc.com>
2025-04-07 06:01:11 +0000

    add music embed

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -133,6 +133,12 @@ const config: Config = {
             },
             {
               html: `
+              <embed src="//music.163.com/style/swf/widget.swf?sid=26494698&type=2&auto=1&width=320&height=66" width=340 height=86 allowNetworking="all">
+              </embed>
+            `,
+            },
+            {
+              html: `
               <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86
                 src="//music.163.com/outchain/player?type=2&id=26494698&auto=1&height=66">
               </iframe>

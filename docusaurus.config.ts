d75713407c7c396f0dafdbea56c6527dae54f16e
seeuswk <31580903+seeuswk@users.noreply.github.com>
2025-04-08 14:43:51 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -73,7 +73,7 @@ const config: Config = {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: "Sunyuanhui's blog",
+      title: "Home",
       logo: {
         alt: "My Site Logo",
         src: "img/logo.svg",
